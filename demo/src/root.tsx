aec25ea231499d15d8e18b86fc44334127dcf7f9
febkosq8 <febkosq8@yahoo.com>
2024-11-27 12:24:25 +0000

    move items to root

--- demo/src/root.tsx
+++ demo/src/root.tsx
@@ -1,13 +1,31 @@
 import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "react-router";
 import "./index.css";
 import "@rinzai/zen/dist/style.css";
+
+export function meta() {
+	return [
+		{ title: "local-save | Demo" },
+		{
+			name: "description",
+			content: "Lightweight wrapper around IndexedDB for secure and structured client-side data storage",
+		},
+	];
+}
+export function links() {
+	return [
+		{
+			rel: "icon",
+			href: "/src/assets/favicon.png",
+			type: "image/png",
+		},
+	];
+}
 export function Layout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en">
 			<head>
 				<meta charSet="UTF-8" />
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-				<title>Local Save</title>
 				<Meta />
 				<Links />
 			</head>
